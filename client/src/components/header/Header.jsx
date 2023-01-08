bc45e6f11d6098307a3c3e7f16cd3af13447a702
Nicholas igunbor <nickoklaus5@gmail.com>
2023-01-08 13:56:57 +0100

    stopped at 21.06

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import './header.css';
 import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faBed, faPlane, faTaxi, faCar} from '@fortawesome/free-solid-svg-icons';
+import {faBed, faPlane, faTaxi, faCar, faCalendarDays, faPerson} from '@fortawesome/free-solid-svg-icons';
 const Header = () => {
   return (
     <div className='header'>
@@ -29,9 +29,25 @@ const Header = () => {
         </div>
         <h1 className="headerTitle">A lifetime of discounts? Its Genius</h1>
         <p className="headerDesc">
-            Get rewarded for your travels unlock instant savings of 10px or more with a free Klausbooking account
+            Get rewarded for your travels - unlock instant savings of 10px or more with a free Klausbooking account
         </p>
         <button className="headerBtn">Sign in / Register</button>
+        <div className="headerSearch">
+            <div className="headerSearchItem">
+                <FontAwesomeIcon icon={faBed} className='headerIcon' />
+                <input type='text' placeholder='Where are you going?'
+                className='headerSearchInput'
+                />
+            </div>
+            <div className="headerSearchItem">
+                <FontAwesomeIcon icon={faCalendarDays} className='headerIcon' />
+                <span className='headerSearchText'>date to date</span>
+            </div>
+            <div className="headerSearchItem">
+                <FontAwesomeIcon icon={faPerson} className='headerIcon' />
+                <span className='headerSearchText'>2 adults 2 children 1 room</span>
+            </div>
+        </div>
         </div>
     </div>
   )
