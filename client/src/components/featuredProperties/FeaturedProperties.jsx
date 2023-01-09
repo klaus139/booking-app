e61c58648cb092a4e7308fcdcf5271b8ea2c937a
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2023-01-09 23:22:07 +0100

    created the featured list component

---
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,67 @@
+import "./featuredProperties.css";
+import { images } from '../../constants';
+
+const FeaturedProperties = () => {
+  return (
+    <div className="fp">
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotel Stare Miasto</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src={images.raddison}
+          alt="radison hotels"
+          className="fpImg"
+        />
+        <span className="fpName">Raddison Hotels and Resort</span>
+        <span className="fpCity">Lagos</span>
+        <span className="fpPrice">Starting from $140</span>
+        <div className="fpRating">
+          <button>9.3</button>
+          <span>Exceptional</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src={images.valencia}
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Valencia Hotels</span>
+        <span className="fpCity">Abuja</span>
+        <span className="fpPrice">Starting from $200</span>
+        <div className="fpRating">
+          <button>8.8</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/322658536.jpg?k=3fffe63a365fd0ccdc59210188e55188cdb7448b9ec1ddb71b0843172138ec07&o=&hp=1"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Hilton Garden Inn</span>
+        <span className="fpCity">Berlin</span>
+        <span className="fpPrice">Starting from $105</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProperties;
--- client/src/constants/image.js
+++ client/src/constants/image.js
@@ -1,6 +1,17 @@
 import abuja1 from '../assets/abuja1.jpeg';
-
+import lagos1 from '../assets/lagos1.jpeg';
+import lagos2 from '../assets/lagos2.png';
+import lagos4 from '../assets/lagos4.jpeg';
+import lagos5 from '../assets/lagos5.jpeg';
+import valencia from '../assets/valencia.jpeg';
+import raddison from '../assets/raddison.webp';
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    abuja1
+    abuja1,
+    lagos1,
+    lagos2,
+    lagos4,
+    lagos5,
+    valencia,
+    raddison,
 };
