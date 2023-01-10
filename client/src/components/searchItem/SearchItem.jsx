ca97b54d914b1d712865aded585c5e4c6287a97f
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2023-01-10 15:39:11 +0100

    added the hotel component with slider functionality

--- client/src/components/searchItem/SearchItem.jsx
+++ client/src/components/searchItem/SearchItem.jsx
@@ -1,31 +1,41 @@
-import React from 'react';
-import './searchItem.css';
+import "./searchItem.css";
 
 const SearchItem = () => {
   return (
-    <div className='searchItem'>
-        <img src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1"
-        alt=''
-        className='siImg'
-        />
-        <div className='siDesc'>
-            <h1 className='siTitle'>Gwagalada Estate Apartments</h1>
-            <span className='siDistance'>500m from Central Town</span>
-            <span className='siTaxiOp'>Free Airport Taxi</span>
-            <span className='siSubtitle'>
-                Studio Apartment with Air conditioning and parking
-            </span>
-            <span className='siFeatures'>
-                Entire studio + 1 bedroom + 21m² 1 full bed
-            </span>
-            <span className='siCancelOp'>Free cancellation</span>
-            <span className='siCancelSubtitle'>
-                You can cancel later, so lock in this great price today!
-            </span>
+    <div className="searchItem">
+      <img
+        src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1"
+        alt=""
+        className="siImg"
+      />
+      <div className="siDesc">
+        <h1 className="siTitle">Tower Estate Apartments</h1>
+        <span className="siDistance">500m from the Capital</span>
+        <span className="siTaxiOp">Free airport taxi</span>
+        <span className="siSubtitle">
+          Studio Apartment with Air conditioning and spacious parking
+        </span>
+        <span className="siFeatures">
+          Entire studio • 2 bathrooms • 21m² 1 full bed
+        </span>
+        <span className="siCancelOp">Free cancellation </span>
+        <span className="siCancelOpSubtitle">
+          You can cancel later, so lock in this great price today!
+        </span>
+      </div>
+      <div className="siDetails">
+        <div className="siRating">
+          <span>Excellent</span>
+          <button>8.9</button>
         </div>
-        <div className='siDetails'>details</div>
+        <div className="siDetailTexts">
+          <span className="siPrice">$200</span>
+          <span className="siTaxOp">Includes taxes and fees</span>
+          <button className="siCheckButton">See availability</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchItem
+export default SearchItem;
