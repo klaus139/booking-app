fc3b000a042aebee9f4dcea3b14264d00dd62fbc
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2023-01-11 06:59:06 +0100

    controllers and models

--- api/index.js
+++ api/index.js
@@ -1,1 +1,58 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+import authRoute from './routes/auth.js';
+import usersRoute from './routes/users.js';
+import hotelsRoute from './routes/hotels.js';
+import roomsRoute from './routes/rooms.js';
+
+dotenv.config();
+const app = express();
+
+const connect = () => {
+    mongoose.set('strictQuery', false);
+    try{
+        mongoose.connect(process.env.MONGO);
+        console.log('connected to mongoDB');
+    } catch(error){
+        throw error;
+    }
+};
+
+mongoose.connection.on('disconnected',()=>{
+    console.log('mongodb disconnected');
+})
+
+app.get('/', (req, res)=> {
+    res.send('hello world')
+})
+
+//middlewares
+
+app.use(express.json());
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', roomsRoute);
+
+app.use((err, req, res, next)=>{
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || "Something went wrong"
+ 
+    return res.status(500).json({
+        succes: false,
+        status:errorStatus,
+        message: errorMessage,
+        stack: err.stack
+    });
+});
+
+
+
+
+app.listen(8080, ()=>{
+    connect();
+    console.log('connected to backend')
+})
